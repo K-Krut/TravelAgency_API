bb6fe504c038655ff8906b7500033ae7420f9f05
xanasdev <mshuribitov@mail.ru>
2023-10-05 20:41:48 +0300

    Settings Update

--- TravelAgency_API/settings.py
+++ TravelAgency_API/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = "TravelAgency_API.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'G5lGGkod6N7NlsiJAqEJ',
+        'HOST': 'containers-us-west-46.railway.app',
+        'PORT': '7348',
     }
 }
 
--- travel_api/models.py
+++ travel_api/models.py
@@ -65,7 +65,7 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
-    order = models.models.ForeignKey(Order, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
     place_number = models.IntegerField(verbose_name='Номер места')
     name = models.CharField(verbose_name='Имя')
     surname = models.CharField(verbose_name='Фамилия')
