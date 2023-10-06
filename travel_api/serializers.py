eab9846928219e9a753b8ba71ded262860e82055
K-Krut <katiakrut448@gmail.com>
2023-10-06 18:23:31 +0300

    tours images logic changed

--- travel_api/serializers.py
+++ travel_api/serializers.py
@@ -3,10 +3,22 @@ from .models import *
 
 
 class TourSerializer(serializers.ModelSerializer):
-    images = serializers.StringRelatedField(many=True)
+    images = serializers.SerializerMethodField()
     season = serializers.StringRelatedField(many=True)
 
     class Meta:
         model = Tour
-        # fields = ('id', 'tour_image')
         fields = ('id', 'name', 'date_start', 'date_end', 'price', 'free_places', 'season', 'images')
+
+    # TODO нужно возвращать первую существующую картинку, если нет картинки с индикатором ис меин,
+    #  а если картинок вообще нет, то не возвращать поле имеджис (я уже это сделала)
+
+    def get_images(self, obj):
+        main_image = obj.images.filter(is_main=True).first()
+        return main_image.aws_url if main_image else None
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if not representation['images']:
+            representation.pop('images')
+        return representation
