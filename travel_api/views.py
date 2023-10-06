9ce1c176c2278052b0fe949f1be4313824afc912
xanasdev <mshuribitov@mail.ru>
2023-10-06 21:39:17 +0300

    pagination

--- travel_api/views.py
+++ travel_api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.forms import model_to_dict
 from rest_framework import generics
 from django.shortcuts import render
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
@@ -10,6 +11,13 @@ from .models import *
 from .serializers import *
 
 
+class TourPagination(PageNumberPagination):
+    page_size = 2
+    page_size_query_param = 'page_size'
+    max_page_size = 10
+
+
 class ToursList(generics.ListCreateAPIView):
     queryset = Tour.objects.order_by("name", "free_places")
     serializer_class = TourSerializer
+    pagination_class = TourPagination
