2eaa9ea59c51328aec18ca13e1fd23701611a3b3
K-Krut <katiakrut448@gmail.com>
2023-10-18 16:16:02 +0300

    added base files for redefinition admin panel styles + collected static files + added static files settings

--- TravelAgency_API/urls.py
+++ TravelAgency_API/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
 from travel_api import views
 
 urlpatterns = [
@@ -27,3 +29,5 @@ urlpatterns = [
     path('api/v1/tours/<int:id>/', views.DetailsTour.as_view())
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
