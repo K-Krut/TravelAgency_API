962b21eb18c2a147d4ca7260b675160a1e2fa403
K-Krut <katiakrut448@gmail.com>
2023-10-31 23:51:02 +0200

    admin models updated

--- travel_api/management/commands/fake_program.py
+++ travel_api/management/commands/fake_program.py
@@ -3,6 +3,7 @@ from django.core.management.base import BaseCommand
 from faker import Faker
 from travel_api.models import *
 from random import choice
+from .data.additional_options_images import images
 
 fake = Faker()
 
@@ -17,15 +18,22 @@ class Command(BaseCommand):
         num_entries = kwargs['num_entries']
 
         for _ in range(num_entries):
-            tour = choice(Tour.objects.filter(pk=4))
+            tour = choice(Tour.objects.all())
+            # tour = choice(Tour.objects.filter(pk=1))
             day = choice(TourDay.objects.all())
             program_item = TourProgram.objects.filter(tour=tour, tour_days=day).order_by('-order').first()
             order = program_item.order + 1 if program_item and program_item.order else 1
+            landmark = True
+            # landmark = random.choice([True, False])
+            url = random.choice(images) if landmark else None
+
             tour_program = TourProgram.objects.create(
                 tour=tour,
                 tour_days=day,
                 tour_option=choice(TourDayOption.objects.all()),
-                order=order
+                order=order,
+                is_landmark=landmark,
+                image_url=url
             )
 
         self.stdout.write(self.style.SUCCESS(f'Successfully generated {num_entries} fake data entries'))
