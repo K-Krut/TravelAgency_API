7ae9b2f1e7e5076ce0ca68b3d8a045a587145928
K-Krut <katiakrut448@gmail.com>
2023-12-14 00:06:32 +0200

    updated aws s3 data

--- TravelAgency_API/settings.py
+++ TravelAgency_API/settings.py
@@ -153,7 +153,7 @@ BASE_CLIENT_URL = os.getenv('BASE_CLIENT_URL')
 # AWS S3 Storage settings  #############################################################################################
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'travel-agency-test'
+AWS_STORAGE_BUCKET_NAME = 'itour-s3'
 AWS_S3_SIGNATURE_NAME = 's3v4',
 AWS_S3_REGION_NAME = 'eu-north-1'
 AWS_S3_FILE_OVERWRITE = False
