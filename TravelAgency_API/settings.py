3858813c847e0586f5f14df139f85a2123f377aa
K-Krut <katiakrut448@gmail.com>
2023-12-22 20:53:41 +0200

    programs for tours data uploaded

--- TravelAgency_API/settings.py
+++ TravelAgency_API/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'ta-travel-agency-api-1bae69c42c2d.herokuapp.com',
